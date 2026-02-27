53b9dd5bb7b79e9bb24f8646b39d2d139d395545
Mina R <minarumang@icloud.com>
2026-02-27 14:34:33 +0100

    message works

--- server.ts
+++ server.ts
@@ -1,2 +1,15 @@
 import express from "express";
 const app = express();
+
+//set the port
+const PORT = 3000;
+
+//set up a basic route
+app.get("/", (req, res) => {
+  res.send("Welcome to our API!");
+});
+
+// start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port: http://localhost:3000/.`);
+});
